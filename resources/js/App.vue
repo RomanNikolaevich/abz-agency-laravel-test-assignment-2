94a282681132c5bf55626b11dcdf11e79d3c1ee1
roman_kobzarov <backend1974@gmail.com>
2024-12-09 15:01:33 +0200

    feat: add utility components to App

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,11 +1,17 @@
 <template>
     <div id="app">
+        <HeaderMenu />
         <router-view></router-view>
+        <ScrollToTop />
     </div>
 </template>
 
 <script>
+import HeaderMenu from "@/views/components/HeaderMenu.vue";
+import ScrollToTop from "@/views/components/ScrollToTop.vue";
+
 export default {
     name: 'App',
+    components: {ScrollToTop, HeaderMenu},
 }
 </script>
